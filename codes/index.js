9da4afc6b0a43f773b1b55529a821ad117be3f6d
ifaronti <135629496+ifaronti@users.noreply.github.com>
2023-12-25 01:17:02 +0000

    remaining form validation and mobile layout

--- codes/index.js
+++ codes/index.js
@@ -1,7 +1,13 @@
 let nameInput = document.getElementById('name')
-let cardNumber = document.getElementById('number')
+let cardNumber = document.getElementById('card-number')
 let theBtn = document.querySelector('.form-btn')
-let monthInput = document.getElementById('month') 
+let monthInput = document.getElementById('month')
+let yearInput = document.getElementById('year')
+let cvc = document.getElementById('cvc')
+let nameError = document.getElementById('name-error')
+let numberError = document.getElementById('number-error')
+let dateError = document.getElementById('date-error')
+let cvcError = document.getElementById('cvc-error')
 
 nameInput.addEventListener('input', function(){
     let cardName = document.querySelector('.card-name')
@@ -11,6 +17,21 @@ nameInput.addEventListener('input', function(){
 cardNumber.addEventListener('input', function(){
     let cardValue = document.querySelector('.cardNumber')
     cardValue.innerHTML = cardNumber.value
+})
+
+monthInput.addEventListener('input', function(){
+    let cardMonth = document.querySelector('.month')
+    cardMonth.innerHTML = monthInput.value + "/"
+})
+
+yearInput.addEventListener('input', function(){
+    let cardYear = document.querySelector('.year')
+    cardYear.innerHTML = yearInput.value
+})
+
+cvc.addEventListener('input', function(){
+    let cardCVC = document.querySelector('.cvc-number')
+    cardCVC.innerHTML = cvc.value
 })
 
 // theBtn.addEventListener('click', function(event){
